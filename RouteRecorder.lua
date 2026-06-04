89b07fd69cb94fb0f98ac35f69b5cdb398820e4e
fabrici3102h-png <fabrici3102h@gmail.com>
2026-06-04 15:25:45 -0400

    Update RouteRecorder.lua

--- RouteRecorder.lua
+++ RouteRecorder.lua
@@ -1,1 +1,15 @@
-print("Olá, GitHub!")
+local ok, err = pcall(function()
+
+    local gui = Instance.new("ScreenGui")
+    gui.Parent = game:GetService("CoreGui")
+
+    local label = Instance.new("TextLabel")
+    label.Parent = gui
+    label.Size = UDim2.new(0, 300, 0, 50)
+    label.Text = "ROUTE RECORDER CARREGADO"
+
+end)
+
+if not ok then
+    warn(err)
+end
